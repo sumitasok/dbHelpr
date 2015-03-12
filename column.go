d0008ee633c0924fc6cbb9e34a1b44b31e53987a
Sumit Asok <sumitasok@gmail.com>
2015-03-12 23:01:01 +0530

    working code

--- column.go
+++ column.go
@@ -1,6 +1,29 @@
 package dbhelpr
 
+import (
+	"reflect"
+	"strings"
+)
+
+type DbDetails struct {
+	TagIdentifier string
+}
+
 // ColumnName will return the name of the column in mysql database
-func ColumnName(table interface{}) string {
-	return ""
+func (d *DbDetails) ColumnName(table interface{}) string {
+	field := "Field"
+	tagValue, ok := reflect.TypeOf(table).FieldByName(field)
+	if ok != true {
+		panic("Field with name '" + field + "'' Doesn't exist")
+	}
+	tagStr := tagValue.Tag.Get(d.TagIdentifier)
+	sep := ","
+	index := 0
+	for i, tag := range strings.Split(tagStr, sep) {
+		if i == index {
+			return tag
+		}
+	}
+
+	panic("tag not found! for " + field)
 }
--- column_test.go
+++ column_test.go
@@ -6,13 +6,17 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type tTable struct {
-	Field string `mysql:"column_name"`
-}
-
 func TestColumnNameSuccess(t *testing.T) {
 	assert := assert.New(t)
 
-	assert.Equal("column_name", ColumnName(tTable{}))
+	type tTable struct {
+		Field string `mysql:"column_name"`
+	}
+
+	d := DbDetails{
+		TagIdentifier: "mysql",
+	}
+
+	assert.Equal("column_name", d.ColumnName(tTable{}))
 	assert.True(true)
 }
