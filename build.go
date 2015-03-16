9279f0136d0649b3e5a7bdb9f35b93305e2583d0
Sumit Asok <sumitasok@gmail.com>
2015-03-17 02:01:17 +0530

    build with time added

--- build.go
+++ build.go
@@ -2,6 +2,7 @@ package dbhelpr
 
 import (
 	"reflect"
+	"regexp"
 	"strings"
 	"time"
 )
@@ -14,15 +15,11 @@ func (db Db) Build(r Resource) {
 	numberOfColumns := reflect.TypeOf(r).NumField()
 
 	queryString := "INSERT INTO " + r.ResourceName() + " (" + listOfTags(r) + ") VALUES(" + valueQuery(numberOfColumns) + ")"
-	println(queryString)
-	stmtCreate, sCError := db.Instance.Prepare(queryString)
-	x := []interface{}{
-		"eventID", "venueID", "venueName", time.Now().UTC(), time.Now().UTC(),
-	}
-	stmtCreate.Exec(x...)
+
+	_, sCError := db.Instance.Exec(queryString, listOfValues(r)...)
 
 	if sCError != nil {
-		db.Logger.Fatal(sCError)
+		println(sCError.Error())
 	}
 }
 
@@ -38,6 +35,35 @@ func listOfTags(r Resource) string {
 		columnList = append(columnList, tagList[0])
 	}
 	return strings.Join(columnList, ",")
+}
+
+func listOfValues(r Resource) []interface{} {
+	n := reflect.TypeOf(r).NumField()
+	columnList := []interface{}{}
+	for i := 0; i < n; i++ {
+		// Check and Fill time.Time
+		tagStr := reflect.TypeOf(r).FieldByIndex([]int{i}).Tag.Get("mysql")
+		tagList := strings.Split(tagStr, ",")
+		if len(tagList) != 0 {
+			if condition, _ := regexp.MatchString(".*_at", tagList[0]); condition {
+				if v, ok := reflect.ValueOf(r).FieldByIndex([]int{i}).Interface().(time.Time); ok {
+					const layout = "0001-01-01 00:00:00 +0000 UTC"
+					if v.String() == layout {
+						columnList = append(columnList, time.Now().UTC())
+					} else {
+						value := reflect.ValueOf(r).FieldByIndex([]int{i}).Interface()
+						columnList = append(columnList, value)
+					}
+				}
+			} else {
+				value := reflect.ValueOf(r).FieldByIndex([]int{i}).Interface()
+				columnList = append(columnList, value)
+			}
+		}
+
+	}
+	return columnList
+
 }
 
 func valueQuery(n int) string {
--- build_test.go
+++ build_test.go
@@ -1,5 +1,7 @@
 package dbhelpr
 
+// Adds date time for tags .*_at as now() when not provided
+
 import (
 	"github.com/stretchr/testify/assert"
 	"testing"
@@ -10,15 +12,17 @@ type Venue struct {
 	EventID   string    `mysql:"event_id,false"`
 	ID        string    `mysql:"venue_id"`
 	Name      string    `mysql:"venue_name"`
-	UpdatedAt time.Time `mysql:"updated_at"`
-	CreatedAt time.Time `mysql:"created_at"`
+	UpdatedAt time.Time `mysql:"venue_updated_at"`
+	CreatedAt time.Time `mysql:"venue_created_at"`
 }
 
 var (
-	venue = Venue{
-		EventID: "event id",
-		ID:      "venue id",
-		Name:    "Melbourn",
+	yesterday = time.Now().AddDate(0, 0, -1).UTC()
+	venue     = Venue{
+		EventID:   "event id",
+		ID:        time.Now().String(),
+		Name:      "Melbourn",
+		CreatedAt: yesterday,
 	}
 )
 
@@ -35,9 +39,12 @@ func TestBuildSuccess(t *testing.T) {
 		venueResult := Venue{}
 
 		result := dbCp.Instance.QueryRow("select * from event_venue where venue_id = ?", venue.ID)
-		result.Scan(venueResult.EventID, venueResult.ID, venueResult.Name, venueResult.CreatedAt, venueResult.UpdatedAt)
+		result.Scan(&venueResult.EventID, &venueResult.ID, &venueResult.Name, &venueResult.CreatedAt, &venueResult.UpdatedAt)
 
 		assert.Equal(venue.Name, venueResult.Name)
+		assert.Equal(venue.EventID, venueResult.EventID)
+		// datetime remains same when provided
+		// assert.Equal(yesterday.String(), venueResult.CreatedAt.String())
 	})
 
 	assert.True(true)
